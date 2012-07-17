e3c09a7aa59fe73b080e37e11d54cf1e59c2406b
Graham Bell <g.bell@jach.hawaii.edu>
2012-07-16 16:29:00 -1000

    Adding email notification sender.

---
+++ lib/crab/notify/mail.py
@@ -0,0 +1,40 @@
+# It appears we can't call this file lib/crab/notify/email.py because
+# it would then hide the default 'email' pacakge.
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
+from smtplib import SMTP
+
+from crab.report.text import report_to_text
+from crab.report.html import report_to_html
+
+class CrabNotifyEmail:
+    """Class to send notification messages by email."""
+
+    def __init__(self, config, base_url):
+        """Construct a nofication object.
+
+        Stores relevant configuration information in the object."""
+
+        self.home = config['crab']['home']
+        self.server = config['email']['server']
+        self.from_ = config['email']['from']
+        self.base_url = base_url
+
+    def __call__(self, report, output, to):
+        """Sends a report by email to the given addresses."""
+
+        message = MIMEMultipart('alternative')
+        message['Subject'] = 'Crab notification'
+        message['Date'] = formatdate(localtime=True)
+        message['From'] = self.from_
+        message['To'] = ', '.join(to)
+
+        message.attach(MIMEText(report_to_text(report, output), 'text'))
+        message.attach(MIMEText(report_to_html(report, output,
+                                self.home, self.base_url), 'html'))
+
+        smtp = SMTP(self.server)
+        smtp.sendmail(self.from_, to, message.as_string())
+        smtp.quit()
