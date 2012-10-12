66f847b5e2abbe594481882eb367b61ef3e688f8
Graham Bell <g.bell@jach.hawaii.edu>
2012-10-12 12:11:41 -1000

    Templ, JS: added hide/show all to crontabs page.
    There are now switches at the end of the crontabs
    page to show or hide all of the raw crontabs, and
    the job tables.
    
    Also prevented the "show deleted" switch from appearing
    if there are no deleted jobs.

--- res/crontabs.js
+++ res/crontabs.js
@@ -16,6 +16,7 @@ $(document).ready(function () {
     $('[id^="show_deleted_"]').click(function (event) {
         var crontab = event.target.id.replace('show_deleted_', '');
         $('.deleted_' + crontab).show();
+        $('#table_' + crontab).show();
         $('#show_deleted_' + crontab).hide();
         $('#hide_deleted_' + crontab).show();
         event.preventDefault();
@@ -25,6 +26,39 @@ $(document).ready(function () {
         $('.deleted_' + crontab).hide();
         $('#show_deleted_' + crontab).show();
         $('#hide_deleted_' + crontab).hide();
+        event.preventDefault();
+    });
+    $('#show_all_raw').click(function (event) {
+        $('[id^="raw_"]').show();
+        $('[id^="show_raw_"]').hide();
+        $('[id^="hide_raw_"]').show();
+        $('#show_all_raw').hide();
+        $('#hide_all_raw').show();
+        event.preventDefault();
+    });
+    $('#hide_all_raw').click(function (event) {
+        $('[id^="raw_"]').hide();
+        $('[id^="show_raw_"]').show();
+        $('[id^="hide_raw_"]').hide();
+        $('#show_all_raw').show();
+        $('#hide_all_raw').hide();
+        event.preventDefault();
+    });
+    $('#show_all_table').click(function (event) {
+        $('[id^="table_"]').show();
+        $('[class^="deleted_"]').hide();
+        $('[id^="show_deleted_"]').show();
+        $('[id^="hide_deleted_"]').hide();
+        $('#show_all_table').hide();
+        $('#hide_all_table').show();
+        event.preventDefault();
+    });
+    $('#hide_all_table').click(function (event) {
+        $('[id^="table_"]').hide();
+        $('[id^="show_deleted_"]').show();
+        $('[id^="hide_deleted_"]').hide();
+        $('#show_all_table').show();
+        $('#hide_all_table').hide();
         event.preventDefault();
     });
 });
