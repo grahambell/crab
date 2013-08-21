24f166a4cba0c8eff2956bbf7cb55809dd95495b
Graham Bell <g.bell@jach.hawaii.edu>
2013-08-20 15:51:49 -1000

    JS: make invert optional with ANSI colors.
    There is now a separate button which can be used to invert the
    colors if necessary.

--- res/coloroutput.js
+++ res/coloroutput.js
@@ -13,16 +13,29 @@ $(document).ready(function () {
         $('.joboutput').each(function (index) {
             var joboutput = $(this);
             if (joboutput.html().search(/\033\[/) != -1) {
+                var colorinvert = 0;
                 var colorpara = $.parseHTML('<p></p>');
+                var invertpara = $.parseHTML('<p class="hidden"></p>');
                 var colorlink = $.parseHTML(
                     '<a href="#"><span class="icon-adjust"></span> Display ANSI colors.</a>');
+                var invertlink = $.parseHTML(
+                    '<a href="#"><span class="icon-exchange"></span> Invert colors.</a>');
                 joboutput.before(colorpara);
+                joboutput.before(invertpara);
                 $(colorpara).append(colorlink);
+                $(invertpara).append(invertlink);
                 $(colorlink).click(function (event) {
-                    $(colorlink).hide();
-                    joboutput.parent().css('color', 'white');
-                    joboutput.parent().css('background-color', 'black');
+                    $(colorpara).hide();
+                    $(invertpara).show();
                     joboutput.html(ansi_to_html(joboutput.html()));
+                    event.preventDefault();
+                });
+                $(invertlink).click(function (event) {
+                    colorinvert = colorinvert ? 0 : 1;
+                    var fg = colorinvert ? 'white' : 'black';
+                    var bg = colorinvert ? 'black' : 'white';
+                    joboutput.parent().css('color', fg);
+                    joboutput.parent().css('background-color', bg);
                     event.preventDefault();
                 });
             }
